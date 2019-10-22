872de00fad3f9045dd00572a2496d254526bd025
Dumbledore <headmaster@hogwartslibrary.com>
2019-10-22 19:22:27 -0400

    Update: - Updated challenge response messages. - Added new html pages for challenges. - Added logging and name identity to logs.
    Signed-off-by: Dumbledore <headmaster@hogwartslibrary.com>

--- src/Posts/challenge_two.py
+++ src/Posts/challenge_two.py
@@ -6,6 +6,7 @@
 def check_flag(flag_guess):
     actual_flag = "S1i2!Fs9pl"
     if flag_guess.lower() == actual_flag.lower():
-        return "You have gotten the correct flag. Please move onto the next challenge."
+        return "You have gotten the correct flag. Please move onto the next challenge. " \
+               "www.hogwartslibrary.com:777/iUhS5m1Nap7vU"
     else:
         return "This flag is incorrect. Please try again."
--- src/hogwarts.py
+++ src/hogwarts.py
@@ -8,14 +8,15 @@ import Plugins.geo_ip as geo
 import Plugins.news as news
 import Plugins.quote as quote
 import Plugins.weather as weather
-import Posts.challenge_four as c4
 import Posts.challenge_one as c1
 import Posts.challenge_three as c3
 import Posts.challenge_two as c2
 from flask import Flask, render_template, request, send_file
 
-logging.basicConfig(filename='API.log', level=logging.INFO,
-                    format='%(levelname)s: %(asctime)s %(message)s', datefmt='%m-%d-%Y %I:%M:%S %p')
+logging.basicConfig(filename='API.log', level=logging.DEBUG,
+                    format='%(name)s -- %(levelname)s: %(asctime)s %(message)s', datefmt='%m-%d-%Y %I:%M:%S %p')
+gf = logging.getLogger('GF')
+gen = logging.getLogger('GEN')
 
 app = Flask(__name__)
 
@@ -33,20 +34,20 @@ def load_splash_page():
 @app.route('/api/geo_ip/', methods=['POST'])
 def IP():
     IP_ADDR = request.form['IP']
-    logging.info('Geo IP accessed by: ' + request.remote_addr)
-    logging.info(request.remote_addr + ' ran GEO IP with the IP address: ' + IP_ADDR)
+    gen.info('Geo IP accessed by: ' + request.remote_addr)
+    gen.info(request.remote_addr + ' ran GEO IP with the IP address: ' + IP_ADDR)
     return geo.main(IP_ADDR)
 
 
 @app.route('/api/weather/', methods=['GET'])
 def Weather():
-    logging.info('Weather accessed by: ' + request.remote_addr)
+    gen.info('Weather accessed by: ' + request.remote_addr)
     return weather.main()
 
 
 @app.route('/api/news/', methods=['GET'])
 def News():
-    logging.info('News accessed by: ' + request.remote_addr)
+    gen.info('News accessed by: ' + request.remote_addr)
     return news.main()
 
 
@@ -54,7 +55,7 @@ def News():
 def Quote():
     QoD = dict()
     QoD['Quote'], QoD['Author'] = quote.main()
-    logging.info('Quote accessed by: ' + request.remote_addr)
+    gen.info('Quote accessed by: ' + request.remote_addr)
     return QoD
 
 
@@ -89,36 +90,53 @@ def send_text_file():
 
 
 @app.route('/fpiS3GQz4Zx99')
-def return_gf_page():
+def return_c1_page():
     """
-    The Grey Fellows Induction Home Page
+    The Grey Fellows Induction Challenge 1 Page
     :return:
     """
-    return render_template("gf_page.html")
+    return render_template("challenge_1.html")
+
+
+@app.route('/lvpalS7qqDL9W')
+def return_c2_page():
+    """
+    The Grey Fellows Induction Challenge 2 Page
+    :return:
+    """
+    return render_template("challenge_2.html")
+
+
+@app.route('/iUhS5m1Nap7vU')
+def return_c3_page():
+    """
+    The Grey Fellows Induction Challenge 3 Page
+    :return:
+    """
+    return render_template("challenge_3.html")
 
 
 @app.route('/api/0EHEOgBABoeLR', methods=['POST'])
 def check_c1():
-    logging.info(request.form['UniqueID'] + ' ran a C1 check with the flag: ' + request.form['Flag'])
+    gf.info(request.form['ID'] + ' at IP address: ' + request.remote_addr + ' ran a C1 check with the flag: ' +
+            request.form['Flag'])
     return c1.check_flag(request.form['Flag'])
 
 
 @app.route('/api/W4qu75hmCXVJE', methods=['POST'])
 def check_c2():
-    logging.info(request.form['UniqueID'] + ' ran a C2 check with the flag: ' + request.form['Flag'])
+    gf.info(request.form['ID'] + ' at IP address: ' + request.remote_addr + ' ran a C2 check with the flag: ' +
+            request.form['Flag'])
     return c2.check_flag(request.form['Flag'])
 
 
 @app.route('/api/4dhSX6b839wJr', methods=['POST'])
 def check_c3():
-    logging.info(request.form['UniqueID'] + ' ran a C3 check with the flag: ' + request.form['Flag'])
+    gf.info(request.form['ID'] + ' at IP address: ' + request.remote_addr + ' ran a C3 check with the flag: ' +
+            request.form['Flag'])
     return c3.check_flag(request.form['Flag'])
 
 
-@app.route('/api/GDloAXlt0jsJZ', methods=['POST'])
-def check_c4():
-    logging.info(request.form['UniqueID'] + ' ran a C4 check with the flag: ' + request.form['Flag'])
-    return c4.check_flag(request.form['Flag'])
 
 
 if __name__ == "__main__":
