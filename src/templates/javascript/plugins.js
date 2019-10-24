32d3c8fce2f05d9fb8bf4f6d976c4d0e35e8ea4c
Dumbledore <headmaster@hogwartslibrary.com>
2019-10-24 12:31:08 -0400

    Update - Created CSS file - Added function to API calls in javascript to replace the text with the API response - Added onload to HTML to run Quote and Weather
    Signed-off-by: Dumbledore <headmaster@hogwartslibrary.com>

--- src/templates/javascript/plugins.js
+++ src/templates/javascript/plugins.js
@@ -15,21 +15,64 @@ function IP_LOOKUP() {
         .catch(error => console.error(error));
 
     function save_data(data) {
-        IP_INFO = data;
+        let City = JSON.stringify(["City"]);
+        let Continent = JSON.stringify(data["Continent"]);
+        let Country = JSON.stringify(data["Country"]);
+        let IP = JSON.stringify(data["IP"]);
+        let ISP = JSON.stringify(data["ISP"]);
+        let Mobile = JSON.stringify(data["Is a mobile device"]);
+        let Latitude = JSON.stringify(data["Latitude"]);
+        let Longitude = JSON.stringify(data["Longitude"]);
+        let Region = JSON.stringify(data["Region Name"]);
+        let DNS = JSON.stringify(data["Reverse DNS Lookup"]);
+        let Time = JSON.stringify(data["Time Zone"]);
+        let Zip = JSON.stringify(data["Zip"]);
+        //document.getElementById('IP_INFO').style.display = 'none'
+        document.getElementById('IP_City').innerHTML = 'City: ' + City.substring(1, City.length - 1);
+        document.getElementById('IP_Continent').innerHTML = 'Continent: ' + Continent.substring(1, Continent.length - 1);
+        document.getElementById('IP_Country').innerHTML = 'Country: ' + Country.substring(1, Country.length - 1);
+        document.getElementById('IP_IP').innerHTML = 'IP: ' + IP.substring(1, IP.length - 1);
+        document.getElementById('IP_ISP').innerHTML = 'ISP: ' + ISP.substring(1, ISP.length - 1);
+        document.getElementById('IP_Mobile').innerHTML = 'Is this a mobile device: ' + Mobile.substring(1, Mobile.length - 1);
+        document.getElementById('IP_Latitude').innerHTML = 'Latitude: ' + Latitude.substring(1, Latitude.length - 1);
+        document.getElementById('IP_Longitude').innerHTML = 'Longitude: ' + Longitude.substring(1, Longitude.length - 1);
+        document.getElementById('IP_Region').innerHTML = 'Region: ' + Region.substring(1, Region.length - 1);
+        document.getElementById('IP_DNS').innerHTML = 'Reverse DNS: ' + DNS.substring(1, DNS.length - 1);
+        document.getElementById('IP_Time').innerHTML = 'Time Zone: ' + Time.substring(1, Time.length - 1);
+        document.getElementById('IP_Zip').innerHTML = 'Zip Code: ' + Zip.substring(1, Zip.length - 1);
     }
 
 }
 
 function WEATHER() {
-    let weather = null;
     fetch('/api/weather/')
         .then(response => response.json())
         .then(data => save_data(data)) // Result from the `response.json()` call
         .catch(error => console.error(error));
 
     function save_data(data) {
-        weather = data;
-        console.log(weather)
+        let today = JSON.stringify(data['0']['detailedForecast']);
+        let tonight = JSON.stringify(data['1']['detailedForecast']);
+        let tomorrow = JSON.stringify(data['2']['detailedForecast']);
+        document.getElementById('Today').innerHTML = today.substring(1, today.length - 1);
+        document.getElementById('Tonight').innerHTML = tonight.substring(1, tonight.length - 1);
+        document.getElementById('Tomorrow').innerHTML = tomorrow.substring(1, tomorrow.length - 1);
+    }
+
+
+}
+
+function QoD() {
+    fetch('/api/quote/')
+        .then(response => response.json())
+        .then(data => save_data(data)) // Result from the `response.json()` call
+        .catch(error => console.error(error));
+
+    function save_data(data) {
+        let author = JSON.stringify(data['Author']);
+        let quote = JSON.stringify(data['Quote']);
+        document.getElementById('QoD_Quote').innerHTML = quote.substring(1, quote.length - 1);
+        document.getElementById('QoD_Author').innerHTML = "-" + author.substring(1, author.length - 1);
     }
 
 
